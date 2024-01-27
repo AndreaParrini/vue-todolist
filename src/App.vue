7f23ef3aba308a8484fe9be5dbcbb68b7d72f602
AndreaParrini <andrea.parrini26@gmail.com>
2024-01-27 10:17:54 +0100

    add insert item rom btn and input add message error

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ export default {
   name: "App",
   data() {
     return {
+      newItem: '',
+      errorMessage: false,
       toDoList: [
         {
           text: "Pagare Bollette",
@@ -41,6 +43,19 @@ export default {
       } else {
         this.toDoList[index].done = true;
       } */
+    },
+    addItem(newItem) {
+      if (newItem.length > 4) {
+        this.errorMessage = false;
+        this.toDoList.push({
+          text: newItem,
+          done: false
+        });
+        this.newItem = "";
+      } else {
+        this.errorMessage = true;
+      }
+
     }
   }
 }
@@ -50,8 +65,21 @@ export default {
 <template>
   <div class="container">
     <h1>To Do List</h1>
+    <div>
+      <input type="text" v-model="newItem" @keyup.enter="addItem(newItem)">
+      <button class=" btn button" @click="addItem(newItem)">
+        Aggiungi
+      </button>
+    </div>
+    <div v-if="errorMessage">
+      <span class="text-danger">
+        <i class="fa-solid fa-triangle-exclamation"></i>
+        Devi inserire ameno 5 caratteri
+        <i class="fa-solid fa-triangle-exclamation"></i>
+      </span>
+    </div>
     <ul v-if="toDoList.length > 0">
-      <li v-for="(item, index) in   toDoList  ">
+      <li v-for="( item, index ) in    toDoList   ">
         <span :class="{ 'line-through': item.done }">{{ item.text }}</span>
         <span>
           <i class="fa-solid fa-square-xmark" @click="remove(index)"></i>
